0d8f12320765c0c1f6d468ceaa7ae68f18daa43c
Tarun Kumar <tarun@stitch3d.com>
2023-03-11 00:57:06 +0100

    [sync-dirs] feat: remove files that are not in src

--- sync_dirs/sync.py
+++ sync_dirs/sync.py
@@ -3,15 +3,36 @@ import sys
 from pathlib import Path
 
 
+def copy_missing_files(src: Path, dst: Path):
+    """Copies files to `dst` that are only in `src`"""
+    src_files = set(file.name for file in src.iterdir())
+    dst_files = set(file.name for file in dst.iterdir())
+
+    files_only_in_src = src_files - dst_files
+
+    for file in files_only_in_src:
+        shutil.copyfile(src / file, dst / file)
+
+
+def remove_redundant_files(src: Path, dst: Path):
+    """Deletes files that are in `dst` but not in `src"""
+    src_files = set(file.name for file in src.iterdir())
+    dst_files = set(file.name for file in dst.iterdir())
+
+    files_only_in_dst = dst_files - src_files
+
+    for file in files_only_in_dst:
+        (dst / file).unlink()
+
+
 def sync(dir1: Path, dir2: Path) -> None:
-    dir_files = lambda dir: [file.name for file in dir.iterdir()]
-    dir1_files = set(dir_files(dir1))
-    dir2_files = set(dir_files(dir2))
-
-    files_in_dir1_only = dir1_files - dir2_files
-
-    for file in files_in_dir1_only:
-        shutil.copyfile(dir1 / file, dir2 / file)
+    """Synchronises two directories.
+    1. If a file exists in dir1 but not in dir2, it should be copied over ✅
+    2. If a file exists in dir1 but has a different name in dir2, rename the file in dir2 to match ❌
+    3. If a file exists in dir2 but not in dir1, it should be deleted ✅
+    """
+    copy_missing_files(dir1, dir2)
+    remove_redundant_files(dir1, dir2)
 
 
 def main():
--- sync_dirs/test_sync.py
+++ sync_dirs/test_sync.py
@@ -30,3 +30,30 @@ def test_copies_file_to_other_directory(tmp_path: Path):
     assert (target_dir / "virus1.txt").exists()
     assert (target_dir / "virus2.txt").exists()
     assert (target_dir / "virus3.txt").exists()
+
+
+def test_removes_files_that_are_not_in_source(tmp_path: Path):
+    """If a file exists in destination but not in source, it should be removed"""
+
+    # > monk
+    monk_dir = tmp_path / "monk"
+    monk_dir.mkdir()
+
+    # > fool
+    #   > attachment1.txt
+    #   > attachment2.txt
+    fool_dir = tmp_path / "fool"
+    fool_dir.mkdir()
+    (fool_dir / "attachment1.txt").touch()
+    (fool_dir / "attachment2.txt").touch()
+
+    # fool discovers attachments through introspection
+    assert (fool_dir / "attachment1.txt").exists()
+    assert (fool_dir / "attachment2.txt").exists()
+
+    # monk trains fool
+    sync(monk_dir, fool_dir)
+
+    # fool should not have any attachments anymore
+    assert not (fool_dir / "attachment1.txt").exists()
+    assert not (fool_dir / "attachment2.txt").exists()
